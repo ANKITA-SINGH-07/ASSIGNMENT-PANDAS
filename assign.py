8e9ba70461fffc7d91d3d6d3d848b37e3c8273be
ANKITA-SINGH-07 <62071934+ANKITA-SINGH-07@users.noreply.github.com>
2020-10-23 19:00:14 +0530

    Add files via upload

--- assign.py
+++ assign.py
@@ -3,9 +3,11 @@ import pycountry_convert as pc
 
 class StackOverFlowAnalysis():
     def __init__(self, input_file):
-        self.data=pd.read_csv(input_file, encoding='unicode_escape')
+        self.data=pd.read_csv(input_file, encoding='unicode_escape',error_bad_lines=False)
+        self.data['continent']=self.data.apply(lambda x:pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(x['Country'].replace("Hong Kong (S.A.R)","Hong Kong"),cn_name_format="default")),axis=1)
     def avgAgeOfDeveleoper(self):
         try:
+            self.data['Age1stCode'] = pd.to_numeric(self.data.Age1stCode, errors='coerce')
             avgAge=int(self.data['Age1stCode'].mean())
             print("Average age of Developer :{}".format(avgAge))
         except Exception as e:
@@ -22,7 +24,6 @@ class StackOverFlowAnalysis():
             print("Exception/Error occured:{}".format(e))
     def avgSalOfDeve(self,continent='All'):
         try:
-            self.data['continent']=self.data.apply(lambda x: pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(x['Country'], cn_name_format="default")),axis=1)
             if continent=='All':
                 res=self.data.fillna(0).groupby('continent')['ConvertedComp'].mean()
             else:
@@ -35,26 +36,14 @@ class StackOverFlowAnalysis():
     def hobbyAsCode(self,continent="All"):
     #Returns distribution of people who code as hobby.
         try:
-            
-            self.data['continent']=self.data.apply(lambda x: pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(x['Country'], cn_name_format="default")),axis=1)
             if continent=='All':
-                code=self.data.fillna(0).groupby("continent ")[(self.data['Hobbyist']=='Yes')].value_counts()
+                code=self.data[self.data["Hobbyist"]=='Yes'].groupby(['continent','Gender']).agg('count')['Respondent']
             else:
-                code=self.data[self.data['continent']==continent].fillna(0).groupby('continent')[(self.data['Hobbyist'] =='Yes')].value_counts()
+                code=self.data[self.data["Hobbyist"]=='Yes']& self.data[self.data['continent']=='continent'].groupby(['continent','Gender']).agg('count')['Respondent']
             print(code)
-
         except Exception as e:
             print("Exception/Error occured:{}".format(e))
-    def hobbyAsCodeGenderWise(self):
-    #Returns distribution of people who code as hobby based on gender.
-        try:
-          
-                counter=self.data.fillna(0).groupby("Gender")[(self.data['Hobbyist'] =='Yes')].value_counts()
-                print(counter)
-            
-            
-        except Exception as e:
-            print("Exception/Error occured:{}".format(e)) 
+
     def mostPopularLanguage(self):
         #Returns most popular language people wish to learn in year 2020
         ques3_data = self.data['LanguageDesireNextYear'].dropna() 
@@ -72,27 +61,25 @@ class StackOverFlowAnalysis():
     def jobSatisfacton(self,continent="All"):
     #Returns Job Satisfaction of Developers based on continent
         try:
-            
-            self.data['continent']=self.data.apply(lambda x: pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(x['Country'], cn_name_format="default")),axis=1)
             if continent=='All':
-                code=self.data.fillna(0).groupby("continent ")['JobSat'].value_counts()
+                res=self.data.fillna(0).groupby("continent ")['JobSat'].value_counts()
             else:
-                code=self.data[self.data['continent']==continent].fillna(0).groupby('continent')['JobSat'].value_counts()
-            print(code)
-
+                res=self.data[self.data['continent']==continent].fillna(0).groupby('continent')['JobSat'].value_counts()
+            print(res)
         except Exception as e:
             print("Exception/Error occured:{}".format(e))    
 
              
 if __name__ =='__main__':
-    a=StackOverFlowAnalysis("ankita_10_records.csv")
+    a=StackOverFlowAnalysis("survey_results_public - Copy.csv")
     a.avgAgeOfDeveleoper()
     a.pythonDeveForCountry("Kingdom")
     a.avgSalOfDeve("AS")
-    a.hobbyAsCode("AS")
-    a.hobbyAsCodeGenderWise("Man")
-    a.mostPopularLanguage()
-    a.jobSatisfacton()
+    a.hobbyAsCode()
+  
+   #a.hobbyAsCodeGenderWise("Man")
+    #a.mostPopularLanguage()
+    #a.jobSatisfacton()
 
         
 
